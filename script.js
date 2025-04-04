673cadb103853303f7245218d9f58fcad16df901
ala-kad <kaddechiala@gmail.com>
2025-04-04 16:29:55 +0100

    fix: incremented scrolltop value

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function() {topFunction()};
 
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
